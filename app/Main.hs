e0a6bcd3cd30389685be5c9711541f2ba65a249f
Tom Sydney Kerckhove <syd@fpcomplete.com>
2018-11-15 11:51:20 +0100

    first commit

---
+++ app/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import Tui
+
+main :: IO ()
+main = tui
---
+++ src/Tui.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Tui where
+
+import System.Directory
+
+import Brick.AttrMap
+import Brick.Main
+import Brick.Types
+import Brick.Widgets.Core
+import Graphics.Vty.Input.Events
+
+tui :: IO ()
+tui = do
+    initialState <- buildInitialState
+    endState <- defaultMain tuiApp initialState
+    print endState
+
+data TuiState =
+    TuiState
+    deriving (Show, Eq)
+
+type ResourceName = String
+
+tuiApp :: App TuiState e ResourceName
+tuiApp =
+    App
+        { appDraw = drawTui
+        , appChooseCursor = showFirstCursor
+        , appHandleEvent = handleTuiEvent
+        , appStartEvent = pure
+        , appAttrMap = const $ attrMap mempty []
+        }
+
+buildInitialState :: IO TuiState
+buildInitialState = pure TuiState
+
+drawTui :: TuiState -> [Widget ResourceName]
+drawTui _ts = []
+
+handleTuiEvent :: TuiState -> BrickEvent n e -> EventM n (Next TuiState)
+handleTuiEvent s e =
+    case e of
+        VtyEvent vtye ->
+            case vtye of
+                EvKey (KChar 'q') [] -> halt s
+                _ -> continue s
+        _ -> continue s
